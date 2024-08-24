70157444d4255db1c9b60e2e93270f1b1524113c
VishnuRathore98 <rathorevishnu666@gmail.com>
2024-08-24 08:49:52 +0530

    Add: 1. eslint,      2. Problem solving patterns -> frequency Counter

---
+++ Section 4: Problem Solving Patterns/Problem_solving_patterns.md
@@ -0,0 +1,13 @@
+<h3>Problem Solving Patterns</h3>
+
+<a href="https://cs.slides.com/colt_steele/problem-solving-patterns">Slides</a>
+
+<ol>
+<li>Frequency Counter</li>
+<li>Multiple Pointer</li>
+<li>Sliding Window</li>
+<li>Divide and Conquer</li>
+<li>Dynamic Programming</li>
+<li>Greedy Algorithms</li>
+<li>Backtracking</li>
+</ol>
---
+++ Section 4: Problem Solving Patterns/frequencyCounter.js
@@ -0,0 +1,59 @@
+/* 
+ * Frequency Counter
+  -------------------
+
+ * This pattern uses "objects" or "sets" to collect values/frequencies.
+ * This can be used to avoid the need for nested loops or "O(n^2)" operations with arrays/strings.
+*/
+
+/*
+ Write a function called same, which accepts two arrays as input.
+ The function should return true if every value in the array 
+ has it's corresponding value squared in the second array.
+ The frequency of values must be the same.
+*/
+
+// Approach 1: Time complexity: O(n^2)
+
+/*
+function same(arr1, arr2){
+    if (arr1.length !== arr2.length) {
+        return false;
+    }
+    for (const val of arr1) {
+        let index = arr2.indexOf(val**2);
+        if (index === -1) {
+            return false;
+        }
+        arr2.slice(index,1);
+    }
+    return true;
+}
+*/
+
+// Approach 2: Time complexity: O(n)
+
+function same(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  let frequencyCounter1 = {};
+  let frequencyCounter2 = {};
+  for (const val of arr1) {
+    frequencyCounter1[val] = (frequencyCounter1[val] || 0) + 1;
+  }
+  for (const val of arr2) {
+    frequencyCounter2[val] = (frequencyCounter2[val] || 0) + 1;
+  }
+  for (const key in frequencyCounter1) {
+    if (!(key ** 2 in frequencyCounter2)) {
+      return false;
+    }
+    if (frequencyCounter1[key] !== frequencyCounter2[key ** 2]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+console.log(same([1, 2, 3], [4, 1, 4]));
